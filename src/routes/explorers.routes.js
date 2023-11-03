ba6343f6fbc0122219156eceef28850b9cefcdc1
Kerjo <1683284@cstj.qc.ca>
2023-11-03 16:36:43 -0400

    Route de connexion implémenté

--- src/routes/explorers.routes.js
+++ src/routes/explorers.routes.js
@@ -15,7 +15,29 @@ class ExplorersRoutes {
         // router.put('/:idExplorer', explorerValidators.partial(), validator, this.put);
         router.get('/:idExplorer', this.getOne);
         // router.post('/', explorerValidators.complete(), validator, this.post); // Ajout d'un explorer
+        router.post('/actions/login', this.login);
       }
+
+      // Route pour la connexion
+      async login(req, res, next) {
+        const { email, username, password } = req.body;
+        if((email && username) || email === "" || username === "") {
+            return next(HttpError.BadRequest(''));
+        }
+
+        const result = await explorerRepository.login(email, username, password);
+        if(result.explorer) {
+            // Nous sommes connectés
+            let explorer = result.explorer.toObject({getters:false, virtuals:false});
+            explorer = explorerRepository.transform(explorer);
+            const tokens = explorerRepository.generateJWT(explorer.email);
+            res.status(201).json({explorer, tokens});
+        } else {
+            // Erreur lors de la connexion
+            return next(result.err);
+        }
+    }
+
 }
 
 new ExplorersRoutes();
