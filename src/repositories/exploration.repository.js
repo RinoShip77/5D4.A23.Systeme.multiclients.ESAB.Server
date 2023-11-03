57224fbc66b125ef94c653590b87f25ed470c31e
Kerjo <1683284@cstj.qc.ca>
2023-11-03 17:39:50 -0400

    Create & transform exploration implemented

--- src/repositories/exploration.repository.js
+++ src/repositories/exploration.repository.js
@@ -10,6 +10,23 @@ class ExplorationRepository {
         return retrieveQuery;
     }
 
+    // Création d'une exploration
+    create(exploration) {
+        return Exploration.create(exploration);
+    }
+
+    // Permet de retirer les imformations sesnsibles d'une exploration et créer son href avant de le retourner
+    // TODO: Vérifier si on a besoin
+    transform(exploration, transformOptions = {}) {
+
+        exploration.href = `${process.env.BASE_URL}/explorations/${exploration._id}`;
+
+        delete exploration._id;
+        delete exploration.__v;
+
+        return exploration;
+
+    }
 }
 
 export default new ExplorationRepository();
