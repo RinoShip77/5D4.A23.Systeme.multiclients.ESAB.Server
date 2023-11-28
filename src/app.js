d22ff8536aef1b0874f981762fab90606379d79b
Ethzac <1871862@cstj.qc.ca>
2023-11-28 17:09:09 -0500

    ajout de cross

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import expressRateLimit from 'express-rate-limit';
 
 //import core
@@ -15,6 +16,7 @@ const app = express();
 database();
 
 app.use(express.json());
+app.use(cors());
 
 //app.use(route);
 
